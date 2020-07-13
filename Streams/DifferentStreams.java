120328bd0ca889ba6b2595c99d9be318e2ff2203
Bartek Walczak <50672367+bl4keee@users.noreply.github.com>
2020-07-13 21:17:59 +0200

    Initial Commit

---
+++ Streams/DifferentStreams.java
@@ -0,0 +1,98 @@
+package Streams;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class DifferentStreams {
+    public static void main(String[] args) throws IOException {
+
+        // 1. Integer Stream
+        IntStream
+                .range(1, 10)
+                .forEach(System.out::print);
+        System.out.println();
+
+        // 2. Integer Stream with skip
+        IntStream
+                .range(1, 10)
+                .skip(5)
+                .forEach(x -> System.out.println(x));
+        System.out.println();
+
+        // 3. Integer Stream with sum
+        System.out.println(
+                IntStream
+                .range(1, 5)
+                .sum());
+        System.out.println();
+
+        // 4. Stream.of, sorted and findFirst
+        Stream.of("Ava", "Aneri", "Alberto")
+                .sorted()
+                .findFirst()
+                .ifPresent(System.out::println); // Alberto
+
+        // 5. Stream from Array, sort, filter and print
+        String[] names = {"Al", "Ankit", "Kushal", "Brent", "Sarika", "amanda", "Hans", "Shivika"};
+        Arrays.stream(names) // same as Stream.of(names)
+                .filter(x -> x.startsWith("S"))
+                .sorted()
+                .forEach(System.out::println); // Sarika, Shivika
+
+        // 6. Average of squares of an int array
+        Arrays.stream(new int[] {2, 4, 6, 8, 10})
+                .map(x -> x * x)
+                .average()
+                .ifPresent(System.out::println); // 44.0
+
+        // 7. Stream from List, filter and print
+        List<String> people = Arrays.asList("Al", "Ankit", "Kushal", "Brent", "Sarika", "amanda", "Hans", "Shivika");
+        people
+                .stream()
+                .map(String::toLowerCase)
+                .filter(x -> x.startsWith("a"))
+                .forEach(System.out::println); // al, ankit, amanda
+
+        // 8. Stream rows from text file, sort, filter and print
+        Stream<String> bands = Files.lines(Paths.get("bands.txt"));
+        bands
+                .sorted()
+                .filter(x -> x.length() > 13)
+                .forEach(System.out::println);
+        bands.close();
+
+        // 9. Stream rows from text file and save to List
+        List<String> bands2 = Files.lines(Paths.get("bands.txt"))
+                .filter(x -> x.contains("jit"))
+                .collect(Collectors.toList());
+        bands2.forEach(x -> System.out.println(x));
+
+        // 10. Stream rows from CSV file and count
+        Stream<String> rows1 = Files.lines(Paths.get("data.txt"));
+        int rowCount = (int)rows1
+                .map(x -> x.split(","))
+                .filter(x -> x.length == 3)
+                .count();
+        System.out.println(rowCount + " rows.");
+        rows1.close();
+
+        // 11. Reduction - sum
+        double total = Stream.of(7.3, 1.5, 4.8)
+                .reduce(0.0, (Double a, Double b) -> a + b);
+        System.out.println("Total = " + total); // 13.6000...
+
+        // 12. Reduction - summary statistics
+        IntSummaryStatistics summary = IntStream.of(7, 2, 19, 88, 73, 4, 10)
+                .summaryStatistics();
+        System.out.println(summary); //IntSummaryStatistics{count=7, sum=203, min=2, average=29,000000, max=88}
+        }
+    }
